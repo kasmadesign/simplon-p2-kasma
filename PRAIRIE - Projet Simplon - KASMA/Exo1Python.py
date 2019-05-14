c0312f8d6ab9d1aa978399d31bd0cff31ab33f92
Mamadou Kalidou SALL <49408852+kasmadesign@users.noreply.github.com>
2019-05-14 18:07:23 +0200

    Add files via upload

---
+++ PRAIRIE - Projet Simplon - KASMA/Exo1Python.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+valeur1 = input ("premiere note")
+valeur2 = input("seconde note")
+valeur1= int(valeur1)
+valeur2=int(valeur2)
+moyenne = float((valeur1 + valeur2)/2)
+print ("la moyenne est:", moyenne)
